ed7f96802fea350050fe6d35ea4f03e35b491551
Steve Kane <kanesteven@gmail.com>
2014-10-14 17:06:09 -0500

    OOP style system now defined for updating physics on all appropriate objects in system-example

--- system-example.js
+++ system-example.js
@@ -1,6 +1,7 @@
 let uuid                                     = require("node-uuid")
-let {transducers, functions}                 = require("prodash")
+let {transducers, functions, object}         = require("prodash")
 let {compose, instanceOf}                    = functions
+let {hasKeys, hasKey}                        = object
 let {filtering, plucking, cat, reduce, cons} = transducers
 
 let print = (c) => console.log(JSON.stringify(c, null, 2))
@@ -24,12 +25,36 @@ function addLiving (isLiving, obj) {
   obj.isLiving = isLiving
 }
 
+function addColor (r, g, b, a, obj) {
+  obj.color = new Float32Array([r, g, b, a])
+}
+
+function addDirection (dx, dy, dz, obj) {
+  obj.direction = new Float32Array([dx, dy, dz])
+}
+
+function addIntensity (i, obj) {
+  obj.intensity = i
+}
+
 function Point () {
   if (!(this instanceof Point)) return new Point
   
   makeEntity(this)
   addPosition(1, 1, 1, this)
-  addVelocity(.01, .01, .01, this)
+  addVelocity(0.0, .01, .04, this)
+  addLiving(true, this)
+}
+
+function Light () {
+  if (!(this instanceof Light)) return new Light
+
+  makeEntity(this)
+  addPosition(1, 1, 1, this)
+  addVelocity(0.1, 0.1, 0.5, this)
+  addColor(1.0, 0.0, 0.0, 1.0, this)
+  addDirection(1.0, 1.0, 1.0, this)
+  addIntensity(0.6, this)
   addLiving(true, this)
 }
 
@@ -87,34 +112,48 @@ function flatcat (host, ar) {
   return host
 }
 
+function isArrayLike (ar) {
+  return instanceOf(Array, ar) ||
+         instanceOf(Float32Array, ar) ||
+         instanceOf(Uint32Array, ar)
+}
+
+//N.B. used internally by FlatGraph
+function ArrayPointer (index, length) {
+  if (!(this instanceof ArrayPointer)) return new ArrayPointer(index, val)
+
+  this.index  = index
+  this.length = length
+}
+
 /*
 N.B. internally used by FlatGraph
 create components array if not already defined
 create node ledger if not already defined
 assign index for this node ledger to the current length of the array (the next index)
+define the range of indeces (defaults to 1 for non-array values)
 */
 function appendComponent (fg, componentName, node) {
-  let val            = node[componentName]
+  let val = node[componentName]
 
-  fg.components[componentName]     = fg.components[componentName] || []
-  fg.nodes[node.id]                = fg.nodes[node.id] || {}
-  fg.nodes[node.id][componentName] = fg.components[componentName].length
+  fg.components[componentName] = fg.components[componentName] || []
+  fg.nodes[node.id]            = fg.nodes[node.id] || {}
 
-  if      (instanceOf(Array, val))        flatcat(fg.components[componentName], val)
-  else if (instanceOf(Float32Array, val)) flatcat(fg.components[componentName], val)
-  else if (instanceOf(Uint32Array, val))  flatcat(fg.components[componentName], val)
-  else                                    fg.components[componentName].push(val)
+  if (isArrayLike(val)) {
+    fg.nodes[node.id][componentName] = new ArrayPointer(fg.components[componentName].length, val.length)
+    flatcat(fg.components[componentName], val) 
+  } else {
+    fg.nodes[node.id][componentName] = new ArrayPointer(fg.components[componentName].length, 1)
+    fg.components[componentName].push(val) 
+  }
 }
 
 //N.B. internally used by FlatGraph
 function updateFlatGraph (fg, node) {
   let keys = Object.keys(node)
-  let key
-  let componentIndex
 
   for (var i = 0; i < keys.length; ++i) {
-    key            = keys[i]
-    if (node[key] !== undefined) appendComponent(fg, key, node)
+    appendComponent(fg, keys[i], node)
   }
   return fg
 }
@@ -124,32 +163,70 @@ function FlatGraph (graph) {
 
   this.nodes      = {}
   this.components = {}
-  
-  return reduce(updateFlatGraph, this, graph)
+  reduce(updateFlatGraph, this, graph)
+}
+
+//given a set of conditions, return the indexes that satisfy the condition for each field
+function getIndexPointersWith (fields, flatGraph) {
+  let nodeIds       = Object.keys(flatGraph.nodes)
+  let indexPointers = {}
+  let node    
+
+  for (var j = 0; j < fields.length; ++j) {
+    indexPointers[fields[j]] = {
+      indeces: [],
+      lengths: [] 
+    }
+  }
+
+  for (var i = 0; i < nodeIds.length; ++i) {
+    node = flatGraph.nodes[nodeIds[i]]
+    if (hasKeys(fields, node)) {
+      for (var k = 0; k < fields.length; ++k) {
+        indexPointers[fields[k]].indeces.push(node[fields[k]].index)
+        indexPointers[fields[k]].lengths.push(node[fields[k]].length)
+      }  
+    }
+  }
+  return indexPointers
+}
+
+//Experimental OOP-style System Constructor.  Computes indeces during construction
+function PhysicsSystem (flatGraph) {
+  let indexPointers = getIndexPointersWith(["position", "velocity"], flatGraph)
+
+  this.posIndeces    = indexPointers.position.indeces
+  this.velIndeces    = indexPointers.velocity.indeces
+  this.count         = indexPointers.position.indeces.length
+  this.positions     = flatGraph.components.position
+  this.velocities    = flatGraph.components.velocity
+  this.run           = (dT) => {
+    for (var i = 0; i < this.count; ++i) {
+      this.positions[this.posIndeces[i]]   += (this.velocities[this.velIndeces[i]] * dT)
+      this.positions[this.posIndeces[i]+1] += (this.velocities[this.velIndeces[i]+1] * dT)
+      this.positions[this.posIndeces[i]+2] += (this.velocities[this.velIndeces[i]+2] * dT)
+    }
+  }
 }
 
 let g  = Graph()
 let p1 = Point()
 let p2 = Point()
 let p3 = Point()
+let l1 = Light()
+let l2 = Light()
 
 attachToRoot(g, p1)
-attachToRoot(g, p2)
-attachToNode(g, p1, p3)
+//attachToRoot(g, p2)
+//attachToRoot(g, l1)
+//attachToRoot(g, l2)
+//attachToNode(g, p1, p3)
 
-let fg = FlatGraph(g)
+let fg            = FlatGraph(g)
+var physicsSystem = new PhysicsSystem(fg)
+var i             = 0
 
-print(fg)
-
-//function updatePositions () {
-//  let posI = 0
-//  let velI = 0
-//
-//  for (var i = 0; i < count; ++i) {
-//    posI               = posIndeces[i]
-//    velI               = velIndeces[i]
-//    positions[posI]   += velocities[velI]
-//    positions[posI+1] += velocities[velI+1]
-//    positions[posI+2] += velocities[velI+2]
-//  }
-//}
+for (var m = 0; m < 10; ++m) {
+  physicsSystem.run(.1)
+  console.log(fg.components.position)
+}
