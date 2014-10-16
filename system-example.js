d079582deb419df8867919f81a802ae0e0635143
Steve Kane <kanesteven@gmail.com>
2014-10-16 16:12:35 -0500

    work done on 3d collision detection

--- system-example.js
+++ system-example.js
@@ -1,8 +1,8 @@
-let uuid                                     = require("node-uuid")
-let {transducers, functions, object}         = require("prodash")
-let {compose, instanceOf}                    = functions
-let {hasKeys, hasKey}                        = object
-let {filtering, plucking, cat, reduce, cons} = transducers
+let uuid                             = require("node-uuid")
+let {transducers, functions, object} = require("prodash")
+let {instanceOf}                     = functions
+let {hasKeys, hasKey}                = object
+let {reduce, cons}                   = transducers
 
 let print = (c) => console.log(JSON.stringify(c, null, 2))
 
@@ -25,6 +25,10 @@ function addLiving (isLiving, obj) {
   obj.isLiving = isLiving
 }
 
+function addCollision (doesCollide, obj) {
+  obj.doesCollide = doesCollide
+}
+
 function addColor (r, g, b, a, obj) {
   obj.color = new Float32Array([r, g, b, a])
 }
@@ -35,6 +39,10 @@ function addDirection (dx, dy, dz, obj) {
 
 function addIntensity (i, obj) {
   obj.intensity = i
+}
+
+function addSize (w, h, d, obj) {
+  obj.size = new Float32Array([w, h, d])
 }
 
 function Point () {
@@ -58,6 +66,17 @@ function Light () {
   addLiving(true, this)
 }
 
+function Box (x, y, z, w, h, d) {
+  if (!(this instanceof Box)) return new Box(x, y, z, w, h, d)
+
+  makeEntity(this)
+  addPosition(x, y, z, this)
+  addVelocity(0.1, 0.1, 0.5, this)
+  addSize(w, h, d, this)
+  addColor(1.0, 0.0, 0.0, 1.0, this)
+  addCollision(true, this)
+  addLiving(true, this)
+}
 
 function Graph () {
   if (!(this instanceof Graph)) return new Graph
@@ -105,11 +124,9 @@ function attachToNode (graph, parentNode, node) {
   return attachById(graph, parentNode.id, node)
 }
 
+//seems obvious to do this point-free but having bugs... investigate
 function flatcat (host, ar) {
-  for (var i = 0; i < ar.length; ++i) {
-    host.push(ar[i]) 
-  }
-  return host
+  return reduce(cons, host, ar) 
 }
 
 function isArrayLike (ar) {
@@ -118,7 +135,7 @@ function isArrayLike (ar) {
          instanceOf(Uint32Array, ar)
 }
 
-//N.B. used internally by FlatGraph
+//used internally by FlatGraph
 function ArrayPointer (index, length) {
   if (!(this instanceof ArrayPointer)) return new ArrayPointer(index, val)
 
@@ -127,7 +144,7 @@ function ArrayPointer (index, length) {
 }
 
 /*
-N.B. internally used by FlatGraph
+use internally by FlatGraph
 create components array if not already defined
 create node ledger if not already defined
 assign index for this node ledger to the current length of the array (the next index)
@@ -194,17 +211,71 @@ function getIndexPointersWith (fields, flatGraph) {
 //Experimental OOP-style System Constructor.  Computes indeces during construction
 function PhysicsSystem (flatGraph) {
   let indexPointers = getIndexPointersWith(["position", "velocity"], flatGraph)
+  let posIndeces    = indexPointers.position.indeces
+  let velIndeces    = indexPointers.velocity.indeces
+  let count         = indexPointers.position.indeces.length
+  let positions     = flatGraph.components.position
+  let velocities    = flatGraph.components.velocity
 
-  this.posIndeces    = indexPointers.position.indeces
-  this.velIndeces    = indexPointers.velocity.indeces
-  this.count         = indexPointers.position.indeces.length
-  this.positions     = flatGraph.components.position
-  this.velocities    = flatGraph.components.velocity
-  this.run           = (dT) => {
+  this.run = (dT) => {
+    for (var i = 0; i < count; ++i) {
+      positions[posIndeces[i]]   += (velocities[velIndeces[i]] * dT)
+      positions[posIndeces[i]+1] += (velocities[velIndeces[i]+1] * dT)
+      positions[posIndeces[i]+2] += (velocities[velIndeces[i]+2] * dT)
+    }
+  }
+}
+
+//simple AABB in 3d space -- avoiding allocation fucking sucks
+function overlaps (size1, pos1, size2, pos2) {
+  let [half1x, half1y, half1z] = size1
+  let [half2x, half2y, half2z] = size2
+  let [pos1x, pos1y, pos1z]    = pos1
+  let [pos2x, pos2y, pos2z]    = pos2
+  //lower bounds
+  let lb1x = pos1x - half1x
+  let lb1y = pos1y - half1y
+  let lb1z = pos1z - half1z
+  let lb2x = pos2x - half2x
+  let lb2y = pos2y - half2y
+  let lb2z = pos2z - half2z
+  //upper bounds
+  let ub1x = pos1x + half1x
+  let ub1y = pos1y + half1y
+  let ub1z = pos1z + half1z
+  let ub2x = pos2x + half2x
+  let ub2y = pos2y + half2y
+  let ub2z = pos2z + half2z
+
+  return ((lb2x <= ub1x && ub1x <= ub2x) || (lb1x <= ub2x && ub2x <= ub1x)) &&
+         ((lb2y <= ub1y && ub1y <= ub2y) || (lb1y <= ub2y && ub2y <= ub1y)) &&
+         ((lb2z <= ub1z && ub1z <= ub2z) || (lb1z <= ub2z && ub2z <= ub1z))
+}
+
+function handleCollision (id1, id2) {
+  console.log(id1 + " has collided with " + id2)
+}
+
+/*
+TODO: doesCollide is technically a boolean which COULD be false....would still get picked up here...
+probably indicates that the getIndexPointersWith should be more powerful/flexible for writing
+rules
+
+This is a VERY naive AABB collision system which simply calls a function with the ids
+of the two colliding objects anytime a collision is detected.  This is crappy but proves
+that collision is possible and not insane with the flatgraph structure
+*/
+function CollisionSystem (flatGraph) {
+  let indexPointers = getIndexPointersWith(["position", "size", "doesCollide"], flatGraph)
+
+  this.posIndeces  = indexPointers.position.indeces
+  this.sizeIndeces = indexPointers.size.indeces
+  this.count       = indexPointers.position.indeces.length
+  this.positions   = flatGraph.components.position
+  this.sizes       = flatGraph.components.size
+  this.run         = (dT) => {
+    console.log(indexPointers)
     for (var i = 0; i < this.count; ++i) {
-      this.positions[this.posIndeces[i]]   += (this.velocities[this.velIndeces[i]] * dT)
-      this.positions[this.posIndeces[i]+1] += (this.velocities[this.velIndeces[i]+1] * dT)
-      this.positions[this.posIndeces[i]+2] += (this.velocities[this.velIndeces[i]+2] * dT)
     }
   }
 }
@@ -215,18 +286,28 @@ let p2 = Point()
 let p3 = Point()
 let l1 = Light()
 let l2 = Light()
+let b1 = Box(5, 5, 5, 10, 10, 10)
+let b2 = Box(0, 0, 0, 3, 3, 3)
+let b3 = Box(11, 11, 11, 4, 4, 6)
 
 attachToRoot(g, p1)
-//attachToRoot(g, p2)
-//attachToRoot(g, l1)
-//attachToRoot(g, l2)
-//attachToNode(g, p1, p3)
+attachToRoot(g, p2)
+attachToRoot(g, l1)
+attachToRoot(g, l2)
+attachToRoot(g, b1)
+attachToRoot(g, b2)
+attachToRoot(g, b3)
+attachToNode(g, p1, p3)
 
-let fg            = FlatGraph(g)
-var physicsSystem = new PhysicsSystem(fg)
-var i             = 0
+let fg              = FlatGraph(g)
+var physicsSystem   = new PhysicsSystem(fg)
+var collisionSystem = new CollisionSystem(fg)
 
-for (var m = 0; m < 10; ++m) {
+console.log(overlaps(b1.size, b1.position, b2.size, b2.position))
+console.log(overlaps(b2.size, b2.position, b3.size, b3.position))
+
+for (var m = 0; m < 1; ++m) {
   physicsSystem.run(.1)
-  console.log(fg.components.position)
+  collisionSystem.run(.1)
+  console.log(fg.components.position[2])
 }
