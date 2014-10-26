6c0e6efeae2c6640bcd771adc78d7005db7afe7a
Steve Kane <kanesteven@gmail.com>
2014-10-26 15:14:19 -0500

    last commit with query as function

--- src/modules/entity-store.js
+++ src/modules/entity-store.js
@@ -17,15 +17,6 @@ function EntityStore () {
   this.appendFunctions     = {}
 }
 
-/* Assign component to entity inside the entity store.
- * Check if table for this component exists, if not create it
- * Check if a transform fn is defined for this key
- * Check if an append function is defined for this key
- * If needed, transform the data using the located transform fn
- * and then append the structure onto the table.  
- * Record the starting index in the appropriate node index
- * Record the change in table size as the length in the node index
- */
 function addComponent (es, eid, key, val) {
   var append    = es.appendFunctions[key]   || cons 
   var transform = es.toTableTransforms[key] || passThrough
@@ -33,7 +24,7 @@ function addComponent (es, eid, key, val) {
   var initialLength
   var finalLength
 
-  if (!es.tables[key]) es.tables[key] = []
+  es.tables[key] = es.tables[key] || []
   initialLength  = es.tables[key].length
   es.tables[key] = append(es.tables[key], transform(val))
   finalLength    = es.tables[key].length
@@ -46,15 +37,147 @@ function addComponent (es, eid, key, val) {
 function addEntity (es, e) {
   var tableKeys = Object.keys(e)
   var eid       = es.nodes.length
+  var i         = tableKeys.length
    
   es.nodes.push({})
-  for (var i = 0; i < tableKeys.length; ++i) {
-    addComponent(es, eid, tableKeys[i], e[tableKeys[i]])
-  }
+  while (i--) addComponent(es, eid, tableKeys[i], e[tableKeys[i]])
   return eid
 }
 
-es.EntityStore    = EntityStore
-es.addEntity      = addEntity
-es.addComponent   = addComponent
-module.exports    = es
+function addEntities (es, entities) {
+  var i = entities.length
+
+  while (i--) addEntity(es, entities[i])
+}
+
+function query (es, queryFn, tableName) {
+  var nodeCount = es.nodes.length
+  var result    = {
+    indices: [],
+    lengths: [] 
+  }
+  var node
+
+  for (var i = 0; i < nodeCount; ++i) {
+    node = es.nodes[i]
+    if (queryFn(node)) {
+      result.indices.push(node[tableName].index)
+      result.lengths.push(node[tableName].length)
+    }
+  } 
+  return result
+}
+
+/* A Query is allocated up front and then mutated to contain its latest
+ * result by calling runQuery and supplying a target Entity Store.  The
+ * result is stored on the query object so that all data is pre-allocated
+ * wherever possible avoiding unneeded garbage collection.  
+ *
+ * It is also possible that we will build Query Pools for queries that
+ * need to be run dynamically as part of "tight loops".  This may be needed
+ * for querying a set of objects and then running queries on their children
+ * or parents
+ */
+function Query (options) {
+  if (!options.fetch || !options.where) throw new Error("Bad Query!")
+
+  this.fetch  = options.fetch
+  this.where  = options.where
+  this.count  = options.count || null
+  this.among  = options.range || null
+  this.result = {
+    count:   0,
+    indices: {},
+    lengths: {} 
+  }
+
+  for (var i = 0; i < options.fetch.length; ++i) {
+    this.result.indices[options.fetch[i]] = []
+    this.result.lengths[options.fetch[i]] = []
+  }
+}
+
+function runBoundedQuery (es, query) {
+  var tableName
+  var node
+
+  query.result.count = 0
+
+  for (var i = 0; i < query.among.length; ++i) {
+    node = es.nodes[query.among[i]]
+    if (query.count && query.result.count >= query.count) break
+    if (query.where(node)) {
+      for (var k = 0; k < query.fetch.length; ++k) {
+        tableName = query.fetch[k]
+        query.result.count++
+        query.result.indices[tableName].push(node[tableName].index)
+        query.result.lengths[tableName].push(node[tableName].length)
+      }
+    }
+  }
+  return query
+}
+
+function runUnboundedQuery (es, query) {
+  var tableName
+  var node
+
+  query.result.count = 0
+
+  for (var i = 0; i < es.nodes.length; ++i) {
+    node = es.nodes[i]
+    if (query.count && (query.result.count >= query.count)) break
+    if (query.where(node)) {
+      for (var k = 0; k < query.fetch.length; ++k) {
+        tableName = query.fetch[k]
+        query.result.count++
+        query.result.indices[tableName].push(node[tableName].index)
+        query.result.lengths[tableName].push(node[tableName].length)
+      }
+    }
+  }
+  return query
+}
+
+function runQuery (es, query) {
+  if (query.among) return runBoundedQuery(es, query)
+  else             return runUnboundedQuery(es, query)
+}
+
+function queryMany (es, queryFn, tableNames) {
+  var tableCount = tableNames.length
+  var nodeCount  = es.nodes.length
+  var result     = {
+    indices: {},
+    lengths: {} 
+  }
+  var tableName
+  var node
+
+  for (var i = 0; i < tableCount; ++i) {
+    result.indices[tableNames[i]] = [] 
+    result.lengths[tableNames[i]] = [] 
+  }
+
+  for (var j = 0; j < nodeCount; ++j) {
+    node = es.nodes[j]
+    if (queryFn(node)) {
+      for (var k = 0; k < tableCount; ++k) {
+        tableName = tableNames[k]
+        result.indices[tableName].push(node[tableName].index)
+        result.lengths[tableName].push(node[tableName].length)
+      }
+    }
+  }
+  return result
+}
+
+es.EntityStore  = EntityStore
+es.addEntity    = addEntity
+es.addEntities  = addEntities
+es.addComponent = addComponent
+es.query        = query
+es.queryMany    = queryMany
+es.runQuery     = runQuery
+es.Query        = Query
+module.exports  = es
--- src/modules/tests/entity-store.js
+++ src/modules/tests/entity-store.js
@@ -2,6 +2,11 @@ var test           = require("tape")
 var mod            = require("../entity-store")
 var EntityStore    = mod.EntityStore
 var addEntity      = mod.addEntity
+var addEntities    = mod.addEntities
+var Query          = mod.Query
+var runQuery       = mod.runQuery
+var query          = mod.query
+var queryMany      = mod.queryMany
 var optimizeTables = mod.optimizeTables
 
 function Box (x, y, z, w, h, d) {
@@ -74,4 +79,70 @@ test('addEntity with transform function does correct stuff', function (t) {
   addEntity(es, b2)
   t.plan(1)
   t.same(expected, es.tables.physics)
+})
+
+test('query returns expected lookup information', function (t) {
+  var es = new EntityStore
+  var b1 = new Box(1,1,1,5,5,5)
+  var b2 = new Box(2,2,2,5,5,5)
+  var r  = { name: "Brett Sanders" }
+  var fn = function (e) { return !!e.physics }
+  var q
+
+  addEntities(es, [b1, b2, r])
+  q = query(es, fn, "physics")
+  t.plan(2)
+  t.same(q.indices, [0,1])
+  t.same(q.lengths, [1,1])
+})
+
+test('queryMany returns expected lookup indicies', function (t) {
+  var es = new EntityStore
+  var b1 = new Box(1,1,1,5,5,5)
+  var b2 = new Box(2,2,2,5,5,5)
+  var r  = { name: "Brett Sanders" }
+  var fn = function (e) { return !!e.physics && !!e.size }
+  var q
+
+  addEntities(es, [b1, b2, r])
+  q = queryMany(es, fn, ["physics", "size"])
+  t.plan(4)
+  t.same(q.indices.physics, [0,1])
+  t.same(q.lengths.physics, [1,1])
+  t.same(q.indices.size, [0,1])
+  t.same(q.lengths.size, [1,1])
+})
+
+test('queryChildren returns correct indices', function (t) {
+  var es = new EntityStore
+  var b1 = new Box(1,1,1,5,5,5)
+  var b2 = new Box(2,2,2,5,5,5)
+  var r1 = { name: "Brett Sanders" }
+  var r2 = { name: "Todd Powderstone", parentId: 1 }
+  var fn = function (e) { return !!e.physics && !!e.size }
+  var q
+
+  addEntities(es, [b1, b2, r1, r2])
+  t.plan(1)
+  t.true(true)
+})
+
+test('Query constructor works as expected', function (t) {
+  var es = new EntityStore
+  var b1 = new Box(1,1,1,5,5,5)
+  var b2 = new Box(2,2,2,5,5,5)
+  var r1 = { name: "Brett Sanders" }
+  var r2 = { name: "Todd Powderstone" }
+  var q  = new Query({
+    fetch: ['physics'],
+    where: function (e) { return !!e.physics },
+    count: 2,
+    range: [1,2]  
+  })
+
+  t.plan(1)
+  t.true(true)
+  addEntities(es, [b1, b2, r1, r2])
+  runQuery(es, q)
+  console.log(q)
 })
